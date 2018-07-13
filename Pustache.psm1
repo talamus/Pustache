a3c6e17cec79897dc08569f17e1fcd6b3605417a
Tero Niemi <talamus@gmail.com>
2018-07-13 12:44:11 +0300

    Allow deep nested objects
    and different input object types

--- Pustache.psm1
+++ Pustache.psm1
@@ -1,18 +1,18 @@
 function Get-ScriptDirectory {
-	$Invocation = (Get-Variable MyInvocation -Scope 1).Value
-	Split-Path $Invocation.MyCommand.Path
+	$invocation = (Get-Variable MyInvocation -Scope 1).Value
+	Split-Path $invocation.MyCommand.Path
 }
 
 [Reflection.Assembly]::LoadFile((Join-Path (Get-ScriptDirectory) "\lib\Nustache.Core.dll"))
 function Invoke-Nustache($template, $data) {
-    return [Nustache.Core.Render]::StringToString($template, (Get-Dictionary $data))
+    return [Nustache.Core.Render]::StringToString($template, (ConvertTo-Dictionary $data))
 }
 
-function Get-Dictionary($data) {
-    $dictionary = new-object "System.Collections.Generic.Dictionary``2[[System.String, mscorlib, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089],[System.String, mscorlib, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089]]"
-    $data.keys | %{
-        $dictionary.Add($_, $data.$_)
+function ConvertTo-Dictionary($data) {
+    if ($data.GetType() -ne [string]) {
+        $data = ($data | ConvertTo-Json)
     }
-    return $dictionary
+    $parser = New-Object Web.Script.Serialization.JavaScriptSerializer
+    $parser.MaxJsonLength = $data.length
+    $parser.DeserializeObject($data)
 }
-
